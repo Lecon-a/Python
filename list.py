447638ec1943879cdd3630543d595babf5b65fe8
Sunday P. Afolabi <spog4true@gmail.com>
2022-11-04 22:25:29 -0400

    Copy and format output string

--- list.py
+++ list.py
@@ -11,5 +11,15 @@ fruit.append("tomatoes")
 #display
 print(fruit)
 #clear all element in the list
+'''
 fruit.clear()
 print(fruit)
+'''
+#copy() - this returns a copy of a list
+fruit_2 = fruit.copy()
+print(fruit_2)
+#count() - this counts and returns the total number of times a specified item occur in a given list
+banana_freq = fruit_2.count("banana")
+output = "Banana occurs {} {}."
+freq = "time" if banana_freq <= 1 else "times"
+print(output.format(banana_freq, freq))
