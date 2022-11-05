97fa46fe857f3c78531eedfd73b2d5961564e650
Sunday P. Afolabi <spog4true@gmail.com>
2022-11-04 22:17:24 -0400

    Add: Start learning list method again

---
+++ list.py
@@ -0,0 +1,15 @@
+fruit = ["apple", "banana", "cherry"]
+
+#list method
+#append() add new item to the end of a given list
+fruit.append("orange")
+fruit.append("cucumber")
+fruit.append("corn grain")
+fruit.append("apricots")
+fruit.append("grapes")
+fruit.append("tomatoes")
+#display
+print(fruit)
+#clear all element in the list
+fruit.clear()
+print(fruit)
